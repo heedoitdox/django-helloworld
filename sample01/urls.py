328b29f23d965fac26c44bce6a9fcf3e67591578
youngbin-yun <youngbin@koreaspacedata.com>
2020-10-19 18:06:22 +0900

    first commit

---
+++ sample01/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
+
+import member.api
+
+app_name=’member’
+
+router = routers.DefaultRouter()
+router.register(‘members’, member.api.MemberViewSet)
+
+urlpatterns = [
+ url(r’^admin/’, admin.site.urls),
+ url(r’^api/doc’, get_swagger_view(title=’Rest API Document’)),
+ url(r’^api/v1/’, include((router.urls, ‘member’), namespace=’api’)),
+ ]
